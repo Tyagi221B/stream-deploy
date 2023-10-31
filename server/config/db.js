2a9c5855357fc3d63592321f41a0b3397e20d1b5
Asmit Tyagi <ashmittyagigms@gmail.com>
2023-10-31 20:33:26 +0530

    fgd

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.DATABASE);
+// const connection = mongoose.connect(process.env.DATABASE);
+const connection = mongoose.connect("mongodb+srv://streamon:5SNxzReteHwM8tzt@cluster0.ryuoxmr.mongodb.net/streamon");
 module.exports = { connection };
